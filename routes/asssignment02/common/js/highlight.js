1fb86eaaece8290182ea0102805bb1eecdbe666f
John Salzman <john.r.salzman@gmail.com>
2018-05-27 19:27:49 -0400

    created eyes that look toward highlighted text, still has bugs - case sensitive, linear locating leaves innacurate looking directions, only finds the first <mark> element on the page and not the closest, etxc

--- routes/asssignment02/common/js/highlight.js
+++ routes/asssignment02/common/js/highlight.js
@@ -1,23 +1,25 @@
 const initialHTML = `
-<p>So, I went back and read the instructions myself to see what I'd done wrong. They said:</p>
-
-<blockquote>
-  <p>Remove the tray from the box. Pierce the overwrap several times with a fork and cook on High for 15 minutes, rotating
-    it half way through.</p>
-</blockquote>
-
-<p>I thought he'd told me
-  <em>fifty</em>. No wonder it exploded in my microwave.</p>
-`
+<p>You and I have never met, kind stranger on the internet;</p>
+    <blockquote>
+      <p>our lives have grown in separate ways although we share years, months, weeks, days of passing time - as all who breathe delight from this fine earth do so.
+      </p>
+    </blockquote>
+    <p>
+      Together yet apart from end to starting, birth running into death
+      constrained by momentary choices.
+    </p>
+    <p>Confined upon a chariot of spinning stone glazed lightly verdant (trees sprouting upwards, soil washing down) until seasons
+      dictate unbiquitous chill- prompted change.</p>
+`;
 
 // Function to highlight input text
 const highlightSelectedText = () => {
-  const initQuery = document.getElementById('query-field')
+  const queryField = document.getElementById('query-field')
   const search = document.getElementById('search-input').value;
   const pNodes = document.getElementsByTagName('p');
 
   // Reset for backspace keypress
-  initQuery.innerHTML = initialHTML;
+  queryField.innerHTML = initialHTML;
 
   // Create new RegEx out of input
   const searchRegex = new RegExp(search);
@@ -26,17 +28,80 @@ const highlightSelectedText = () => {
   for (let i in pNodes) {
     const node = pNodes[i];
     // If nodeType is an element,
-    // search is not an empty string,
+    // search is not an empty string OR single space,
     // and the search returns a match index
     if (
       node.nodeType === 1 &&
-      node.innerHTML.search(searchRegex) !== -1 &&
-      search !== ''
+      node.innerText.search(searchRegex) !== -1 &&
+      (search !== '' && search !== ' ')
     ) {
+      // Error: when typing 'ough.' in search
+      //   => changes 3rd <p> 'thought' to highlighted 'ough.'
       node.innerHTML = node.innerHTML.split(searchRegex).join(`<mark>${search}</mark>`)
+      moveEyes(node.innerHTML.search());
+    }
+    else if (search === '') {
+      moveEyes(true)
     }
   }
 }
 
-// for initial load and default text value 'and'
+// for initial load and default text value
 highlightSelectedText()
+
+// Point glimmer in eyes toward first result in search
+// Max [left, top] for right most word 'through.' [34px, 12px]
+// Min [left, top] for left most phrase 'I thought' [12px, 8px]
+// NOTE: Max && Min arbitrarily changed to look better in eyeVal formulas
+function moveEyes(lookback = false) {
+  // if nothing is selected
+  if (lookback) {
+    changeGlimmerPosition(20, 20);
+  }
+  // calculate and set
+  else {
+    // retrieve needed elements
+    const eye = document.getElementsByClassName('eye')[0];
+    const mark = document.getElementsByTagName('mark')[0];
+    const minEl = document.getElementsByTagName('p')[2];
+    const maxEl = document.getElementsByTagName('p')[1];
+
+    // retrieve location information
+    const eyeRect = eye.getBoundingClientRect();
+    const markRect = mark.getBoundingClientRect();
+    const minRect = minEl.getBoundingClientRect();
+    const maxRect = maxEl.getBoundingClientRect();
+
+
+    // define the center of each element in realtion to the page
+    const eyeY = eyeRect.top + 20;
+    const eyeX = eyeRect.left + 20;
+    const markX = (markRect.width / 2) + markRect.left;
+    const markY = (markRect.height / 2) + markRect.top;
+    const minY = minRect.top;
+    const minX = minRect.left;
+    const maxY = maxRect.top;
+    // position to far right, the max, is 'right' property
+    const maxX = maxRect.right;
+
+    // math from picture, but with updated range and base values
+    // to more accurately point in direction of highlighted element
+    const eyeValY = (((markY - minY) / (maxY - minY)) * 4) + 5
+    const eyeValX = (((markX - minX) / (maxX - minX)) * 22) + 14
+
+    changeGlimmerPosition(eyeValY, eyeValX)
+  }
+}
+
+
+// set eye positions for each 'glimmer' element
+function changeGlimmerPosition(top, left) {
+  const glimmer = document.getElementsByClassName('glimmer');
+
+  for (let i in glimmer) {
+    if (i < 2) {
+      glimmer[i].style.top = `${top}px`;
+      glimmer[i].style.left = `${left}px`;
+    }
+  }
+}
